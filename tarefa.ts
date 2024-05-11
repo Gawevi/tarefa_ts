f24a0db404c8a4541078b4af42289bfc49892a48
Gawevi <gbielwade@gmail.com>
2024-05-11 14:24:46 -0300

    Commit inicial

---
+++ tarefa.ts
@@ -0,0 +1,13 @@
+function multiplica(numero1: number, numero2: number): number {
+    return numero1 * numero2;
+}
+
+console.log(multiplica(10, 10));
+
+//////
+
+function dizOi(nome: string): string {
+    return `Olá ${nome}`;
+}
+
+console.log(dizOi("Gabriel"));
